c5c7016d8ee823878d19aaf0389c429f3ac8308f
Ran Guo <rguo2@albany.edu>
2017-04-02 21:58:22 -0400

    method getSize()

--- Queue.java
+++ Queue.java
@@ -3,7 +3,20 @@ public class Queue {
    private Node front;
    private Node last;
    private int size;
-
+   
+   
+    Queue() {
+       front = new Node();
+       last = new Node();
+       front.setNext(last);
+       last.setBack(front);
+       size = 0;
+   }
+    
+     public int getSize() {
+       return size;
+   }
+    
 
 
 
