46d62117965b8523fb2647f61e0728bd940645b5
Ran Guo <rguo2@albany.edu>
2017-04-02 22:07:15 -0400

    add method dequeue()

--- Queue.java
+++ Queue.java
@@ -11,11 +11,11 @@ public class Queue {
        front.setNext(last);
        last.setBack(front);
        size = 0;
-   }
+     }
     
      public int getSize() {
        return size
-   }
+       }
      
        public void enqueue(String string) {
        Node n = new Node();
@@ -26,6 +26,18 @@ public class Queue {
        n.setNext(last);
       
        size++;
+       }
+       
+     public String dequeue() {
+       if(size == 0)
+           return null;
+      
+       Node n = front.getNext();
+      
+       front.setNext(n.getNext());
+       n.getNext().setBack(front);
+       size--;
+       return n.getData();
        }
    }
     
