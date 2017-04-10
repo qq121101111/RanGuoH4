687202bdbbb6767696308338a74cd948411d86dd
Ran Guo <rguo2@albany.edu>
2017-04-09 22:29:02 -0400

    add data

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 public class Main {
 
    public static void main(String[] args) {
-     
-     
+     //add data
+      Stack stack = new Stack();
+       stack.push("A");
+       stack.push("B");
+       stack.push("C");
+       stack.push("E");
+       stack.push("F");
+       stack.push("G");
+       
+       Queue queue = stackToQueue(stack);
      
      
    }
