ff0a964ba99d9bffaedc4eee900def2550928a11
Ran Guo <rguo2@albany.edu>
2017-04-09 22:55:45 -0400

    add queueToStack() and testing

--- Main.java
+++ Main.java
@@ -35,5 +35,30 @@ public class Main {
       
        return queue;
    }
+     static Stack queueToStack(Queue queue) {
+       Queue tempQueue = new Queue();
+      
+       // creating temp stack, as stack elements will be reversed of queue
+       Stack tempStack = new Stack();
+       while(queue.peek() != null) {
+           String s = queue.dequeue();
+           tempQueue.enqueue(s);
+           tempStack.push(s);
+       }
+      
+       // restore on queue
+       while(tempQueue.peek()!=null) {
+           queue.enqueue(tempQueue.dequeue());
+       }
+      
+       // create stack for storing the final result
+       // we will reverse the tempstack on this
+       // so that results are in order
+       Stack stack = new Stack();
+       while(tempStack.peek() != null) {
+           stack.push(tempStack.pop());
+       }
+       return stack;
+   }
 }
     
